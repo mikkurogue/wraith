201c3887d3e846fe63b0dbf9a70a4cae0df2b1b7
mikku <michael.lindemans@outlook.com>
2025-11-14 19:09:00 +0200

    add biome as a linter and formatter and lint stuff

--- index.ts
+++ index.ts
@@ -1,24 +1,29 @@
+/**
+ * Wraith provides utilities for creating and managing opaque types and phantom types in TypeScript.
+ * TODO:
+ * - Add builder support for generating chainable api's for opaque and phantom types.
+ */
 export namespace Wraith {
-  /** 
-   * Makes a type opaque by branding it with a unique signature.
-  */
-  export type Opaque<T, Brand> = T & { __brand: Brand };
-  /** 
-   * A phantom type that carries type information without affecting runtime behavior.
-  */
-  export type Phantom<Tag> = { __phantom: Tag };
+	/**
+	 * Makes a type opaque by branding it with a unique signature.
+	 */
+	export type Opaque<T, Brand> = T & { __brand: Brand };
+	/**
+	 * A phantom type that carries type information without affecting runtime behavior.
+	 */
+	export type Phantom<Tag> = { __phantom: Tag };
 
-  /** 
-   * Creates an opaque type from a given value.
-  */
-  export function makeOpaque<T, Brand>(value: T): Wraith.Opaque<T, Brand> {
-    return value as Wraith.Opaque<T, Brand>;
-  }
+	/**
+	 * Creates an opaque type from a given value.
+	 */
+	export function makeOpaque<T, Brand>(value: T): Wraith.Opaque<T, Brand> {
+		return value as Wraith.Opaque<T, Brand>;
+	}
 
-  /** 
-   * Reveal or unwrap an opaque type to return the original value.
-  */
-  export function reveal<T, Brand>(opaque: Wraith.Opaque<T, Brand>): T {
-    return opaque as T;
-  }
+	/**
+	 * Reveal or unwrap an opaque type to return the original value.
+	 */
+	export function reveal<T, Brand>(opaque: Wraith.Opaque<T, Brand>): T {
+		return opaque as T;
+	}
 }
